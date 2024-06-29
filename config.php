9c95185dd2484b6274761acb0a5c1de7db756941
Ikiscream <33494335+ikii378@users.noreply.github.com>
2024-06-29 23:57:27 +0700

    Update config.php

--- config.php
+++ config.php
@@ -3,41 +3,8 @@
 
 return [
     [
-        'alias' => 'PersadaYamaha',
-        'cookie' => 'PHPSESSID=f61df1f8d989574d9d2ecc9945f31a19'
-    ],
-    [
-        'alias' => 'pklo50',
-        'cookie' => 'PHPSESSID=3ab90b8e174d0650676f61e3256e47a7'
-    ],
-    [
-        'alias' => 'pklo44',
-        'cookie' => 'PHPSESSID=87b50f08e5aaf4a674b165c1ff572035'
-    ],
-    [
         'alias' => 'ikiscream',
-        'cookie' => 'PHPSESSID=0db7147ce57ee38317c9b559a45a4927'
-    ],
-    [
-        'alias' => 'pklo45',
-        'cookie' => 'PHPSESSID=ef16dbbd67c9be26068e6832d8214f9d'
-    ],
-    [
-        'alias' => 'pklo47',
-        'cookie' => 'PHPSESSID=a4919fc195989faf312eb9124b7583c4'
-    ],
-    [
-        'alias' => 'pklo49',
-        'cookie' => 'PHPSESSID=2f5181f8ae0e208989b241e408b33e2c'
-    ],
-    [
-        'alias' => 'pklo46',
-        'cookie' => 'PHPSESSID=4f6e4a07801d37ebcbc20e8886bc15d2'
-    ],
-
-    [
-        'alias' => 'pklo48',
-        'cookie' => 'PHPSESSID=4096d42dcbae3126dcda70bd5134996f'
+        'cookie' => 'PHPSESSID='
     ]
 ];
 ?>
